412bed99006864c8152fdd3b8607b069b8082dc4
Kaashvi Goel <kaashvigoel1508@gmail.com>
2026-04-08 23:58:33 +0530

    Firebase Added

---
+++ lib/features/auth/data/firebase_auth_repo.dart
@@ -0,0 +1,32 @@
+import 'package:social_media_app/features/auth/domain/entities/app_user.dart';
+import 'package:social_media_app/features/auth/domain/repos/auth_repo.dart';
+
+class FirebaseAuthRepo implements AuthRepo {
+  @override
+  Future<AppUser?> loginWithEmailPassword(String email, String password) {
+    // TODO: implement loginWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AppUser?> registerWithEmailPassword(
+    String name,
+    String email,
+    String password,
+  ) {
+    // TODO: implement registerWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logout() {
+    // TODO: implement logout
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AppUser?> getCurrentUser() {
+    // TODO: implement getCurrentUser
+    throw UnimplementedError();
+  }
+}
---
+++ lib/features/auth/domain/entities/app_user.dart
@@ -0,0 +1,17 @@
+class AppUser {
+  final String uid;
+  final String email;
+  final String name;
+
+  AppUser({required this.uid, required this.email, required this.name});
+
+  //convert user --> json
+  Map<String, dynamic> toJson() {
+    return {'uid': uid, 'email': email, 'name': name};
+  }
+
+  //convert json --> user
+  factory AppUser.fromJson(Map<String, dynamic> json) {
+    return AppUser(uid: json['uid'], email: json['email'], name: json['name']);
+  }
+}
